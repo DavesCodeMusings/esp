7cfa718366ac6cc2952d5a556b24f8689862ce2c
Dave <61114342+DavesCodeMusings@users.noreply.github.com>
2022-10-09 16:38:00 -0500

    Update main.py

--- edm-dance-light/main.py
+++ edm-dance-light/main.py
@@ -1,7 +1,7 @@
 # Throbbing EDM Dance Light
 # A pulse-width modulated LED that fades in and out in sync with music.
 from machine import Pin, PWM
-from time import ticks_us, sleep_us
+from time import ticks_diff, ticks_us, sleep_us
 from math import pi, cos
 
 GPIO = 2  # GPIO 2 is the built-in LED on a NodeMCU 8266. Change as needed.
@@ -22,5 +22,5 @@ while (1):
     t += pi / period  # Incrementing time by the beat period syncs the LED to every two beats.
     if (t > 2 * pi):  # 2pi radians is one full cycle of the sine wave.
         t = 0
-    elapsed_time = ticks_us() - start_time
+    elapsed_time = ticks_diff(ticks_us(), start_time)
     sleep_us(1000 - elapsed_time)  # The loop delay is 1 millisecond, which is why beats per ms is important.
