5b52cf3a2fd83f767f4ae28f0b36740283616b0e
Dave <61114342+DavesCodeMusings@users.noreply.github.com>
2023-09-24 13:49:50 -0500

    Update boot.py

--- tourdeus-esp32c3/boot.py
+++ tourdeus-esp32c3/boot.py
@@ -1,18 +1,29 @@
 # You should not need to edit this file. Use settings in config.py instead.
-from machine import Pin
 from config import AP_NAME, AP_PASS, WIFI_TIMEOUT, WEBREPL_PASS
-from network import WLAN, AUTH_WPA2_PSK, STA_IF
-from time import ticks_ms
+from machine import Pin
+from network import WLAN, STA_IF
+from time import ticks_ms, ticks_diff
 from webrepl import start
 
-print('Starting in wifi station mode...')
+p12 = Pin(12, Pin.OUT)  # labeled as D4
+p13 = Pin(13, Pin.OUT)  # labeled as D5
+
+p12.value(1)
+p13.value(1)
+
 wlan = WLAN(STA_IF)
 wlan.active(True)
-wlan.connect(AP_NAME, AP_PASS)
-start_time = ticks_ms()
-while not wlan.isconnected():
-    if (ticks_ms() - start_time > WIFI_TIMEOUT * 1000):
-        break
-if (wlan.isconnected()):
-    oled.text(f'{wlan.ifconfig()[0]}', 0, 56)
+print(f'\nConnecting to {AP_NAME}...')
+if not wlan.isconnected():
+    start_time = ticks_ms()
+    wlan.connect(AP_NAME, AP_PASS)
+    while not wlan.isconnected():
+        if ticks_diff(ticks_ms(), start_time) > WIFI_TIMEOUT * 1000:
+            print('Connection timeout.')
+            break
+
+if wlan.isconnected():
+    print(wlan.ifconfig()[0])
+    p13.value(0)
     start(password=WEBREPL_PASS)
+    p12.value(0)
